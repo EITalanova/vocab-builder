b43245259e2d617ba2360c059a74134bb1dd6b98
EITalanova <elenabelichenko77@gmail.com>
2024-01-22 18:38:41 +0200

    Add btn component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,11 +3,13 @@ import cn from "classnames";
 import style from "./Button.module.scss";
 
 interface IButton {
-
+  variant: 'white' | 'green';
+  text: string;
+  type: 'submit' | 'button';
 }
 
-export const Button: React.FC<IButton> = () => {
+export const Button: React.FC<IButton> = ({ variant, text, type }) => {
   return (
-    <div></div>
+    <button className={cn(style.btn, style[variant])} type={type}>{text}</button>
   );
 };
