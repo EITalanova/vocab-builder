bfa546dd25557288d8d4f15de0a3adfd47bc9309
EITalanova <elenabelichenko77@gmail.com>
2024-01-22 18:12:00 +0200

    Add saas files

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import cn from "classnames";
+
+import style from "./Button.module.scss";
+
+interface IButton {
+
+}
+
+export const Button: React.FC<IButton> = () => {
+  return (
+    <div></div>
+  );
+};
